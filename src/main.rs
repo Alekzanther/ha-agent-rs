5ccbb7bd7c38adda6d3a11baa298d7624c9a92e7
Alekzanther <alexanderlndbrg@gmail.com>
2023-05-11 22:42:53 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use futures::{SinkExt, StreamExt};
 use async_tungstenite::tokio::connect_async;
 use async_tungstenite::tungstenite::protocol::Message;
 
+use std::sync::mpsc;
+
 use url::Url;
 
 #[macro_use]
@@ -24,28 +26,37 @@ async fn main() -> Result<(), anyhow::Error> {
     let hass_token = dotenv!("HASS_TOKEN");
     println!("Home-assistant URL set to: {}", hass_url);
     let url = Url::parse(format!("wss://{}/api/websocket", hass_url).as_str())?;
-    watcher::start().await;
+    let (webcam_state_tx, webcam_state_rx) = mpsc::channel::<bool>();
 
-    // Then, use the `tungstenite` library to connect to the WebSocket URL
-    let (mut ws_stream, _) = connect_async(url)
-        .await
-        .expect("Failed to connect websocket");
-    let _auth_req = ws_stream.next().await.ok_or("Connection closed");
+    let webcam_watcher = watcher::start(webcam_state_tx);
 
-    // Send a message to register the new device
-    let message = Message::text(
-        json!({
-            "type": "auth",
-            "access_token": hass_token
-        })
-        .to_string(),
-    );
-    ws_stream.send(message).await?;
+    loop {
+        let webcam_state = webcam_state_rx.recv().unwrap();
+        if webcam_state {
+            println!("In use!");
+        } else {
+            println!("Not used...")
+        }
+    }
 
-    // Read the response message
-    let response = ws_stream.next().await.ok_or("didn't receive anything");
-    println!("Response: {:?}", response);
-    ws_stream.close(None).await?;
-
-    Ok(())
+    // // Then, use the `tungstenite` library to connect to the WebSocket URL
+    // let (mut ws_stream, _) = connect_async(url).await.expect("Failed to connect websocket");
+    // let _auth_req = ws_stream.next().await.ok_or("Connection closed");
+    //
+    // // Send a message to register the new device
+    // let message = Message::text(
+    //     json!({
+    //         "type": "auth",
+    //         "access_token": hass_token
+    //     })
+    //     .to_string(),
+    // );
+    // ws_stream.send(message).await?;
+    //
+    // // Read the response message
+    // let response = ws_stream.next().await.ok_or("didn't receive anything");
+    // println!("Response: {:?}", response);
+    // ws_stream.close(None).await?;
+    //
+    // Ok(())
 }
--- src/watcher.rs
+++ src/watcher.rs
@@ -1,6 +1,6 @@
-use std::process::Command;
+use std::{process::Command, sync::mpsc::Sender};
 
-use inotify::{EventMask, Inotify, WatchMask};
+use inotify::{Inotify, WatchMask};
 
 fn is_webcam_in_use() -> bool {
     let output = Command::new("lsof")
@@ -11,7 +11,7 @@ fn is_webcam_in_use() -> bool {
     !output.stdout.is_empty()
 }
 
-pub async fn start() {
+pub async fn start(webcam_state_tx: Sender<bool>) {
     let webcam_path = "/dev/video0";
 
     // Create a new inotify instance
@@ -19,6 +19,7 @@ pub async fn start() {
     inotify
         .add_watch(webcam_path, WatchMask::OPEN | WatchMask::CLOSE)
         .expect("Failed to add inotify watch");
+    let mut webcam_in_use = false;
 
     tokio::task::spawn(async move {
         let mut buffer = [0; 1024];
@@ -27,47 +28,10 @@ pub async fn start() {
                 .read_events_blocking(&mut buffer)
                 .expect("Error while reading events");
 
-            for event in events {
-                if is_webcam_in_use() {
-                    println!("In use!")
-                } else {
-                    println!("Not used...")
-                }
-                if event.mask.contains(EventMask::CLOSE_NOWRITE) {
-                    println!("Webcam CLOSE_NOWRITE {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::CLOSE_WRITE) {
-                    println!("Webcam CLOSE_WRITE {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::CREATE) {
-                    println!("Webcam CREATE {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::DELETE) {
-                    println!("Webcam DELETE {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::DELETE_SELF) {
-                    println!("Webcam DELETE_SELF {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::MODIFY) {
-                    println!("Webcam MODIFY {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::MOVE_SELF) {
-                    println!("Webcam MOVE_SELF {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::MOVED_FROM) {
-                    println!("Webcam MOVED_FROM {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::MOVED_TO) {
-                    println!("Webcam MOVED_TO {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::OPEN) {
-                    println!("Webcam OPEN {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::ACCESS) {
-                    println!("Webcam ACCESS {:?}", std::time::Instant::now());
-                }
-                if event.mask.contains(EventMask::IGNORED) {
-                    println!("Webcam ACCESS {:?}", std::time::Instant::now());
+            for _event in events {
+                if is_webcam_in_use() != webcam_in_use {
+                    webcam_in_use = !webcam_in_use;
+                    webcam_state_tx.send(webcam_in_use);
                 }
             }
         }
