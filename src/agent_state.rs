01caaa20f92e6645aa8fbba8c6473d30d9db95e4
Alekzanther <alexanderlndbrg@gmail.com>
2023-05-14 22:24:15 +0200

    Fix metadata

--- src/agent_state.rs
+++ src/agent_state.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 use std::fs;
 use std::io::Error;
 use sys_info::{os_type, os_release, hostname};
-use users::{get_current_uid, get_user_by_uid, User};
+use users::{get_current_uid, get_user_by_uid};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Metadata {
@@ -37,36 +37,38 @@ struct Metadata {
 impl Metadata {
     //init AgentMetadata
     fn init(state_path: &str) -> Self { 
-        //fill fields
-        match load_state(state_path) {
+        //if state_path is empty, return default values
+        match Self::load_state(state_path) {
             Ok(state) => state,
             Err(_) => {
 
-                let os = os_type().unwrap_or_else(_| String::from("Unknown OS"));
+                let os = os_type().unwrap_or_else(|_| String::from("Unknown OS"));
                 let os_version = os_release().unwrap_or_else(|_| String::from("Unknown OS version"));
                 let hostname = hostname().unwrap_or_else(|_| String::from("Unknown Hostname"));
-                let logged_in_user = get_user_by_uid(get_current_uid())
-                        .map(|user| user.name().to_string_lossy().into_owned());
+                let logged_in_user = get_user_by_uid(get_current_uid()).unwrap();
+                let username = logged_in_user.name().to_str().unwrap();
+
+                let device_id = format!("{}@{}", username, hostname);
+
                 Self {
-                    device_id: format!("{}@{}", logged_in_user, hostname),
-                    app_id: "".to_string(),
-                    app_name: "".to_string(),
-                    app_version: "".to_string(),
+                    device_id,
+                    app_id: "ha-agent-rs".to_string(),
+                    app_name: "Home Assistant Agent".to_string(),
+                    app_version: "0.1".to_string(),
                     device_name: hostname,
-                    manufacturer: "".to_string(),
-                    model: "".to_string(),
-                    os_name: "".to_string(),
-                    os_version: "".to_string(),
+                    manufacturer: "Alekzanther".to_string(),
+                    model: "Computer".to_string(),
+                    os_name: os.to_string(),
+                    os_version: os_version.to_string(),
                     supports_encryption: false,
                     app_data: "".to_string(),
                 }
             },
-        };
-            
+        }
     }
 
-    fn save_state(state: &self, path: &str) -> Result<(), Error> {
-        let json = serde_json::to_string_pretty(state)?;
+    fn save_state(state: Self, path: &str) -> Result<(), Error> {
+        let json = serde_json::to_string_pretty(&state)?;
         fs::write(path, json)?;
         Ok(())
     }
